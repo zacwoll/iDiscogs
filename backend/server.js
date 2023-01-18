aa0e31a160a1acaef693816d98b4df2d695b2678
Zac Woll <zacwoll@gmail.com>
2023-01-17 20:34:18 -0600

    Refactored code to be more organized, fixes launch bug

--- backend/server.js
+++ backend/server.js
@@ -332,20 +332,4 @@ app.get('/clearCookies', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
-
-// Cookie utility function
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
+ 
